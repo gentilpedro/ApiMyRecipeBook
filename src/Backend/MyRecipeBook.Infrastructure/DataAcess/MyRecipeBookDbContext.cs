48f5287a80971b5cb517baebd5bcd263065279a5
Pedro Rodrigues <pedro.rodrigues@josapar.com.br>
2026-06-22 10:26:58 -0300

    ➕ Adicionada EntityFrameworkCore no projeto  - Adicionada a classe DbContext no projeto.

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAcess/MyRecipeBookDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAcess;
+
+internal class MyRecipeBookDbContext : DbContext
+{
+    public MyRecipeBookDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+    public DbSet<User> Users { get; set; }
+}
+ 
