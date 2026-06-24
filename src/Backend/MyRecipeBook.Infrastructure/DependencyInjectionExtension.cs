34637ae5aaaef54eb140d9908e44bd50f9245e20
Pedro Rodrigues <pedro.rodrigues@josapar.com.br>
2026-06-23 22:25:33 -0300

    subindo

--- src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -1,5 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repository.User;
 using MyRecipeBook.Domain.Security.PasswordHashing;
+using MyRecipeBook.Infrastructure.DataAcess;
+using MyRecipeBook.Infrastructure.DataAcess.Repository;
 using MyRecipeBook.Infrastructure.Security.PasswordHashing;
 namespace MyRecipeBook.Infrastructure;
 
@@ -7,11 +12,16 @@ public static class DependencyInjectionExtension
 {
     extension(IServiceCollection services)
     {
-        public void AddInfrastructure()
+        public void AddInfrastructure(IConfiguration configuration)
         { 
             services.AddScoped<IPasswordHasher, Argon2PasswordHasher>();
-        
+            services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+            // services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+            services.AddDbContext<MyRecipeBookDbContext>(config =>
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection");  
+                config.UseMySQL(connectionString!);
+            });
         }
     }
-
 }
